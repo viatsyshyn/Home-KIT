616ce59a483dedadbf548d159e7d6070b89bc401
WhiteZombieUA <mshvets@emp.org.ua>
2017-05-19 18:22:41 +0300

    Added date range form

--- smarthome-analytics/routes/home.ts
+++ smarthome-analytics/routes/home.ts
@@ -1,13 +1,18 @@
+import * as dateFormat from "dateformat";
+
 export function home(db) {
     return function (req, res) {
-        const start = new Date(req.query.start || '2017-03-11');
-        const end = new Date(req.query.end || '2017-03-15');
+
+        const date_now = new Date (Date.now());
+        const date_old = new Date (Date.now() - 86400000);
+        const start = new Date(req.query.start || date_now);
+        const end = new Date(req.query.end || date_old);
 
         const x = db.getTemperatureHumidity(start, end, "livingroom-microclimate");
         const y = db.getTemperatureInOut(start, end, "livingroom-heater");
         const z = db.getDeviceOnOff(start, end, "livingroom-heater");
         const v = db.getZoneState(start, end, "livingroom");
-        const r = db.getAirConditioning(start, end, "livingroom-ac")
+        const r = db.getAirConditioning(start, end, "livingroom-ac");
 
         Promise.all([x,y,z,v,r]).then(result => {
 
@@ -27,8 +32,8 @@ export function home(db) {
             }, []);
 
             res.render('index', {
-                start: start,
-                end: end,
+                start: dateFormat(start, "yyyy-mm-dd'T'HH:mm"),
+                end: dateFormat(end, "yyyy-mm-dd'T'HH:mm"),
                 graph1: result[0],
                 graph2: result[1],
                 graph3: result[3],
